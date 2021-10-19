0a879e7ab5b37ebc6244dfb5041cf5aa06b6257b
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2021-10-19 10:21:43 +0530

    fix

--- pagination/script.js
+++ pagination/script.js
@@ -1,33 +1,12 @@
-const getPost = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const dataArrray = await response.json();
-  displayPost(dataArrray);
-  pageCalculation(dataArrray.length);
-};
+const postPerPage = 5;
+let pageNum = 0;
+let start = postPerPage*pageNum;
+let end = start + postPerPage;
 
-getPost();
+let data ;
 
-const displayPost = (data) => {
-  const postContainer = document.querySelector(".post-container");
-  data.forEach((post) => {
-    const postDiv = document.createElement("div");
-    postDiv.innerHTML = `
-       <p> ${post.title}</p>
-        <p>${post.body}</p>
-    `;
-    postDiv.classList.add("post-body");
-    postContainer.appendChild(postDiv);
-  });
-};
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(res => res.json())
+.then(json => window.data=json);
 
-const pageCalculation=(length)=>{
-    const totalPage = Math.ceil(length/10);
-    console.log(totalPage);
-    const pageIndex = document.querySelector('.page-index');
-    for(let i = 0 ; i< totalPage ;i++){
-        const span = document.createElement('span');
-        span.innerHTML=`${i+1}`;
-        span.classList.add('page-number');
-        pageIndex.appendChild(span);
-    }
-}
+
