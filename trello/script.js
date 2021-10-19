9cf05f481e25ebdf4ef74408223a5f752a02a5f7
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2021-10-19 18:41:12 +0530

    adding todo to drag and drop

---
+++ trello/script.js
@@ -0,0 +1,76 @@
+let listItems = document.querySelectorAll('.list-item')
+const jiraContainer = document.querySelectorAll('.jira-container');
+const todoContainer = document.querySelector('#todo')
+const progressContainer=document.querySelector('#progress');
+const completedContainer=document.querySelector('#completed');
+const form =document.querySelector('#form');
+const options={
+    todo : todoContainer,
+    progress : progressContainer,
+    completed : completedContainer
+}
+let draggedItem = null;
+
+const dragged=()=>{
+for(let i = 0 ; i< listItems.length;i++){
+    const item = listItems[i];
+    item.addEventListener('dragstart',()=>{
+        draggedItem = item
+        console.log(draggedItem)
+        setTimeout(()=>{
+            item.style.display='none';
+        },0)
+       
+    })
+
+    item.addEventListener('dragend',()=>{
+        
+        setTimeout(()=>{
+            draggedItem.style.display='block';
+        },0)
+    })
+}
+}
+dragged();
+
+    for(let j = 0 ; j < jiraContainer.length ;j++){
+        const container = jiraContainer[j];
+        container.addEventListener('dragenter',function(e){
+            console.log(`drag enter`);
+            this.style.backgroundColor = 'rgba(0,0,0,0.2)';
+            e.preventDefault();
+        })
+
+        container.addEventListener('dragover',(e)=>{
+            e.preventDefault();
+        })
+
+        container.addEventListener('dragleave',function (e){
+            e.preventDefault();
+            this.style.backgroundColor = '#DC2626';
+        })
+
+        container.addEventListener('drop',function (){
+            this.style.backgroundColor = '#DC2626';
+            this.append(draggedItem);
+        })
+    }
+
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const formdata = new FormData(form);
+    const task = formdata.get('task');
+    const option =formdata.get('option');
+    console.log(task)
+    console.log(option)
+    const div = document.createElement('div')
+    div.classList.add('list-item');
+    div.innerText=task;
+    div.setAttribute('draggable','true');
+    options[option].appendChild(div);
+    listItems=document.querySelectorAll('.list-item');
+    console.log(listItems)
+    dragged();
+    
+})
